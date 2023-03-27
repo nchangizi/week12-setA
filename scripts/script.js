a1d0a8cd86e4e0a913ceec8de2c69b6ad3a099a3
Neda <nedachangizi@gmail.com>
2023-03-27 15:45:48 -0700

    change bg and text

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,24 @@ let buttonRef = document.querySelector("#clickMe");
 
 function alertUser() {
   alert("You Clicked!!!");
-//   buttonRef.removeEventListener("click", alertUser);
-
+  //   buttonRef.removeEventListener("click", alertUser);
 }
 
-buttonRef.addEventListener("click", alertUser,true);
+// buttonRef.addEventListener("click", alertUser, { once: true });
+
+function changeBGPink() {
+  //   document.body.style.backgroundColor = "pink";
+  document.body.classList.add("pinkBG");
+}
+buttonRef.addEventListener("click", changeBGPink);
+
+function changeText() {
+  // if the button says "Click me !" change the textContent to Clicked!
+  if (buttonRef.textContent === "Click Me!") {
+    buttonRef.textContent = "Clicked!";
+  } else {
+    //else if it says "Clicked!" update it to "Click Me!"
+    buttonRef.textContent = "Click Me!";
+  }
+}
+buttonRef.addEventListener("click", changeText);
