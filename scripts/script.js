0aaae9de51970beacb783e842e485217deaa1614
Neda <nedachangizi@gmail.com>
2023-03-27 15:26:03 -0700

    remove event listener after the first time

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ let buttonRef = document.querySelector("#clickMe");
 
 function alertUser() {
   alert("You Clicked!!!");
+  buttonRef.removeEventListener("click", alertUser);
+
 }
 
 buttonRef.addEventListener("click", alertUser);
